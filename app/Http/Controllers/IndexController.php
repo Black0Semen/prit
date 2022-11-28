2abecf53451b7ec05567c9dccb6c455cd8b93ef8
Semen <sem.cheornii@yandex.ru>
2022-11-28 23:49:30 +0500

    first commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class IndexController extends Controller
+{
+    public function mainShow(){
+        return view('home');
+    }
+    public function order(){
+        return view('order');
+    }
+    public function top(){
+        return view('top');
+    }
+}
